6e90ae1e203da6803ee06554d460ff0671c18f8f
Kebap <kebap_spam@gmx.net>
2021-06-09 22:10:19 +0200

    GMCP.Debug steuerbar & deaktiviert

--- src/scripts/krrrcks/GUI/initGMCP.lua
+++ src/scripts/krrrcks/GUI/initGMCP.lua
@@ -1,4 +1,5 @@
 function initGMCP() 
-    sendGMCP( [[Core.Supports.Debug 20 ]])
+    local debugLevel = 0 -- 0 zeigt nichts, 100 zeigt alles, 20 etwas, usw.
+    sendGMCP( f"Core.Debug {debugLevel} ")
     sendGMCP( [[Core.Supports.Set [ "MG.char 1", "MG.room 1", "comm.channel 1" ] ]])
 end
