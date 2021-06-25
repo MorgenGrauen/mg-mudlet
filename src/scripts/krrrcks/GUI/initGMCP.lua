3bad99adc3dd65442cbf2f1fe21c6319ecad9e9b
Matthias Faust <Fausti@users.noreply.github.com>
2021-06-25 12:54:13 +0200

    initGMCP sollte nur noch 1x triggern

--- src/scripts/krrrcks/GUI/initGMCP.lua
+++ src/scripts/krrrcks/GUI/initGMCP.lua
@@ -1,5 +1,7 @@
-function initGMCP() 
-    local debugLevel = 0 -- 0 zeigt nichts, 100 zeigt alles, 20 etwas, usw.
-    sendGMCP( f"Core.Debug {debugLevel} ")
-    sendGMCP( [[Core.Supports.Set [ "MG.char 1", "MG.room 1", "comm.channel 1" ] ]])
+function initGMCP(_, protocol)
+    if protocol == "GMCP" then
+        local debugLevel = 0 -- 0 zeigt nichts, 100 zeigt alles, 20 etwas, usw.
+        sendGMCP( f"Core.Debug {debugLevel} ")
+        sendGMCP( [[Core.Supports.Set [ "MG.char 1", "MG.room 1", "comm.channel 1" ] ]])
+    end
 end
