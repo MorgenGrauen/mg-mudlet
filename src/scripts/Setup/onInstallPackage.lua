e06d95b3485bea95c8a5ed237c98ce9716486189
Matthias Faust <Fausti@users.noreply.github.com>
2021-06-26 10:48:17 +0200

    add: Event für Paketinstallation

---
+++ src/scripts/Setup/onInstallPackage.lua
@@ -0,0 +1,8 @@
+function onInstallPackage(_, packageName)
+    if packageName == "MorgenGrauen" then
+        -- hier kommt alles rein das NUR direkt nach der Installation des MG Pakets
+        -- ausgeführt werden soll
+
+        initGMCP("", "GMCP")
+    end
+end
