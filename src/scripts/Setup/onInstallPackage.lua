977a8a2cd5fc9f005cdd99c06ca37de60d504350
Kebap <kebap_spam@gmx.net>
2021-06-27 00:33:28 +0200

    Karte der Startgebiete laden

--- src/scripts/Setup/onInstallPackage.lua
+++ src/scripts/Setup/onInstallPackage.lua
@@ -4,5 +4,64 @@ function onInstallPackage(_, packageName)
         -- ausgeführt werden soll
 
         initGMCP("", "GMCP")
+
+        -- Karte mit Startgebieten laden
+        -- TODO: Alles folgende in eigenes Skript auslagern, das von hier aufgerufen wird.
+        loadMap(getMudletHomeDir() .. "/map anfaenger v3.dat")
+        -- TODO: Vorab die vorhandene Karte speichern, falls Spieler bereits aktiv war?
+
+        -- Wo befinden wir uns gerade auf der geladenen Karte?
+        local found = false
+        local hash = gmcp.MG.room.info.id -- TODO: prüfen, gibt es das überhaupt schon?
+        local currentRoom
+        local currentHash
+        -- Ganze Karte prüfen, ob wir unseren Hash wiederfinden
+        for id, _ in pairs(getRooms()) do
+          if hash == getRoomHashByID(id) then
+            currentRoom = id
+            currentHash = getRoomHashByID(id)
+            found = true
+          end
+        end
+
+        if found then
+          -- Ah, wir sind in einem Startgebiet. Dann Mapper dorthin ausrichten!
+          mapper.currentHash = currentHash
+          mapper.currentRoom = currentRoom
+          mapper.currentArea = getRoomArea(currentRoom)
+          centerview(currentRoom)
+
+        else
+          -- Sonst irgendwo anders einen neuen Raum anlegen, von dem aus kartographiert werden kann.
+          mapper.currentArea = "world"
+
+          -- Folgendes 1:1 aus neuem Alias "minit" kopiert - TODO: dort und hier in gemeinsame Funktion auslagern!
+
+          -- ersten Raum aus aktuellen GMCP Daten erstellen
+          hash = gmcp.MG.room.info.id
+
+          mapper.currentHash = hash
+
+          local newRoom = createRoom(mapper.currentArea, hash)
+          local roomName = gmcp.MG.room.info.short
+          setRoomName(newRoom, roomName)
+
+          -- im neuen Raum alle sichtbaren Ausgänge prüfen und ggf. Stubs erzeugen
+          for _, exitname in pairs(gmcp.MG.room.info.exits) do
+              addStubExit(newRoom, exitname)
+          end
+
+          mapper.currentRoom = newRoom
+          centerview(newRoom)
+
+          -- Ende "minit"
+        end
+
+        -- wegen initGMCP kommt gleich noch GMCP.MG.room zurück und wir wollen keinen weiteren Raum erstellen!
+        local mode = mapper.mode
+        if not (mode == "fix") then
+          setMapperMode("fix")
+          tempTimer(1, f"setMapperMode('{mode}')")
+        end
     end
 end
