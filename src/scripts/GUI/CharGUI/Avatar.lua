a57006bd71efa3fff4764e812b90760cebcd801a
Kebap <kebap_spam@gmx.net>
2021-07-18 19:43:36 +0200

    Unterordner für Emoji

--- src/scripts/GUI/CharGUI/Avatar.lua
+++ src/scripts/GUI/CharGUI/Avatar.lua
@@ -1,4 +1,4 @@
-local imgPath = "C:/dev/mg-mudlet/src/resources/CharGUI/"
+local imgPath = "C:/dev/mg-mudlet/src/resources/CharGUI/emoji/"
 local AvatarPaths = {
   kaempfer = "2694 Schwerter 512.png",
   abenteurer = "1F9ED Kompass 512.png",
