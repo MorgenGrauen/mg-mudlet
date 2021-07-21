19c00d1989e3cf3fc7b625360b0a780633a39558
Kebap <kebap_spam@gmx.net>
2021-07-21 23:39:28 +0200

    Avatar automatisch aktualisieren

--- src/scripts/GUI/CharGUI/Avatar.lua
+++ src/scripts/GUI/CharGUI/Avatar.lua
@@ -1,4 +1,4 @@
-local imgPath = getMudletHomeDir() .. "/@PKGNAME@/CharGUI/emoji/"
+local imgPath = getMudletHomeDir() .. "/MorgenGrauen/CharGUI/emoji/"
 local AvatarPaths = {
   kaempfer = "2694 Schwerter 512.png",
   abenteurer = "1F9ED Kompass 512.png",
@@ -25,6 +25,20 @@ function initAvatar()
     name = "AvatarLabel",
     x = 0, y = 0, width = "100%", height = "100%", 
   }, GUI.AvatarFrame)
+  GUI.AvatarLabel:setStyleSheet(f[[
+    xborder-color: rgba(113,75,50,100%);
+    xborder-width: 1px; border-style: solid; xborder-radius: 37px;
+    xbackground-color:rgba(200,200,160,20%);
+    qproperty-alignment: AlignCenter;
+  ]])
+  GUI.AvatarLabel:enableClickthrough()
+  registerAnonymousEventHandler("gmcp.MG.char.base.guild", updateAvatar, false)  
+end
+
+function updateAvatar()
+  -- Gilde gewechselt? Sollten wir anzeigen!
+  -- Am besten nur neuen AvatarPath einfügen, Rest beibehalten 
+  -- braucht Vadis CSSMan? https://forums.mudlet.org/viewtopic.php?t=3502
   local AvatarPath = imgPath .. AvatarPaths[ME.gilde] or AvatarPaths["glitzer"]
   GUI.AvatarLabel:setStyleSheet(f[[
     xborder-color: rgba(113,75,50,100%);
@@ -33,12 +47,4 @@ function initAvatar()
     qproperty-alignment: AlignCenter;
     border-image: url("{AvatarPath}");
   ]])
-  GUI.AvatarLabel:enableClickthrough()
-end
-
-function updateAvatar()
-  -- tbd
-  -- Gilde gewechselt? Sollten wir anzeigen!
-  -- nur neuen AvatarPath einfügen, Rest beibehalten 
-  -- braucht Vadis CSSMan? https://forums.mudlet.org/viewtopic.php?t=3502
 end
--- src/scripts/GUI/CharGUI/initCharGUI.lua
+++ src/scripts/GUI/CharGUI/initCharGUI.lua
@@ -12,6 +12,8 @@ function initCharGUI()
   updateFlucht()
 
   initAvatar()
+  updateAvatar()
+  
   initStufe()
   showStufe()
 end
