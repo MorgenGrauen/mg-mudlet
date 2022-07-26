396ac5404fcae5da708e2e91be0f356991c8a77d
Kebap <kebap_spam@gmx.net>
2022-07-26 17:28:44 +0200

    hide obsolete parts of old GUI

--- src/scripts/GUI/initGUI.lua
+++ src/scripts/GUI/initGUI.lua
@@ -2,18 +2,24 @@ function initGUI()
 
   -- Textfenster begrenzen
   setBorderTop(0)
-  setBorderBottom(65) -- bisschen Platz fuer Statuszeile
+  setBorderBottom(25) -- bisschen Platz fuer Statuszeile
   setBorderLeft(0)
   setBorderRight(0)
 
   -- Statuszeile malen. Layout wie folgt:
+  -- Vorher:
   -- Zeile 1: spieler (Name, Stufe), gift, trenner_1, vorsicht (Vorsicht, Fluchtrichtung)
   -- Zeile 2: ort_raum (Region, Raumnummer, Para), ort_region (Ort kurz)
   -- Zeile 3: lp_titel, lp_anzeige (Lebenspunkte-Anzeige), kp_titel, kp_anzeige (KP-Anzeige), trenner_2
 
-  GUI.statuszeile = Geyser.Container:new({name = "statuszeile", x=0, y=-70, width = 600, height=70})
+  -- Bald:
+  -- Zeile 1: ort_raum (Region, Raumnummer, Para), ort_region (Ort kurz)
 
-  -- Zeile 1
+  GUI.statuszeile = Geyser.Container:new({name = "statuszeile", x=0, y=-30, width = 600, height=30})
+
+  --[[ 
+  -- Zeile 1 (vorher)
+  
   GUI.spieler = Geyser.Label:new({
     name = "spieler",
     x = 0, y = -65,
@@ -34,18 +40,23 @@ function initGUI()
     x = 250, y = -65,
     width = 350, height = 20}, GUI.statuszeile)
 
-  -- Zeile 2  
+  -- Zeile 2 (vorher)
+
+  ]]--
+
+  -- Zeile 1 (neu)
   GUI.ort_raum = Geyser.Label:new({
     name = "ort_raum",
-    x = 250, y = -45,
+    x = 250, y = -25,
     width = 350, height = 20}, GUI.statuszeile)
 
   GUI.ort_region = Geyser.Label:new({
     name = "ort_region",
-    x = 0, y = -45,
+    x = 0, y = -25,
     width = 250, height = 20}, GUI.statuszeile)
 
-  -- Zeile 3
+  --[[
+  -- Zeile 3 (vorher)
   GUI.lp_titel = Geyser.Label:new({
     name = "lp_titel",
     x = 0, y = -25,
@@ -62,7 +73,7 @@ function initGUI()
     name = "kp_titel",
     x = 240, y = -25,
     width = 110, height = 20}, GUI.statuszeile)
-  GUI.kp_titel:echo("&nbsp;Konzentration:")
+  GUI.kp_titel:echo("&amp;nbsp;Konzentration:")
 
   GUI.kp_anzeige = Geyser.Gauge:new({
     name = "kp_anzeige",
@@ -74,5 +85,6 @@ function initGUI()
     name = "trenner_2",
     x = 500, y = -25,
     width = 100, height = 20}, GUI.statuszeile)
+  ]]--
 
 end
