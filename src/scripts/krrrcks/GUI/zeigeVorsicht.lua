3c63e9e0d3d94c87896d01af2127542ad9f7fd23
Kebap <kebap_spam@gmx.net>
2022-07-30 19:14:57 +0200

    Optionale Reports berücksichtigen (#27)
    * Fehlenden KP-Report berücksichtigen
    * Fehlende Fluchtrichtung handhaben
    * Warten, bis beide Daten da sind
    * Beide Daten abwarten
    * Beide Daten abwarten

--- src/scripts/krrrcks/GUI/zeigeVorsicht.lua
+++ src/scripts/krrrcks/GUI/zeigeVorsicht.lua
@@ -1,24 +1,25 @@
 function zeigeVorsicht()
 
-  ME.vorsicht = gmcp.MG.char.wimpy.wimpy
-  ME.fluchtrichtung = gmcp.MG.char.wimpy.wimpy_dir
-
-  -- Prinz Eisenherz?
-
-  if ME.vorsicht == 0 then
-    ME.vorsicht = "NIX"
+  if gmcp.MG.char.wimpy and gmcp.MG.char.wimpy.wimpy and gmcp.MG.char.wimpy.wimpy_dir then 
+    ME.vorsicht = gmcp.MG.char.wimpy.wimpy
+    ME.fluchtrichtung = gmcp.MG.char.wimpy.wimpy_dir
+  
+    -- Prinz Eisenherz?
+  
+    if ME.vorsicht == 0 then
+      ME.vorsicht = "NIX"
+    end
+  
+    local zeile = "Vorsicht: " .. ME.vorsicht
+  
+    -- Fluchtrichtung anzeigen, nur wenn gesetzt
+  
+    if ME.fluchtrichtung ~= 0 then
+      zeile = zeile .. ", FR: " .. ME.fluchtrichtung
+    end
+  
+    -- Statuszeile aktualisieren
+  
+    GUI.vorsicht:echo(zeile)
   end
-
-  local zeile = "Vorsicht: " .. ME.vorsicht
-
-  -- Fluchtrichtung anzeigen, nur wenn gesetzt
-
-  if ME.fluchtrichtung ~= 0 then
-    zeile = zeile .. ", FR: " .. ME.fluchtrichtung
-  end
-
-  -- Statuszeile aktualisieren
-
-  GUI.vorsicht:echo(zeile)
-
 end
