3563353488ff78344f76da18a699cb4ef6bbde5d
Kebap <kebap_spam@gmx.net>
2023-01-14 01:04:17 +0100

    neue Funktion: GMCP_Char_deaktivieren (#32)

--- src/scripts/krrrcks/GUI/initGMCP.lua
+++ src/scripts/krrrcks/GUI/initGMCP.lua
@@ -5,3 +5,23 @@ function initGMCP(_, protocol)
         sendGMCP( [[Core.Supports.Set [ "MG.char 1", "MG.room 1", "comm.channel 1" ] ]])
     end
 end
+
+function GMCP_Char_deaktivieren(_)
+    -- gmcp.Char wird bei Login dreimal empfangen: StatusVars, Status, Vitals
+    -- Hintergrund ist, dass Mudlet das automatisch im GMCP Handshake anfordert.
+    -- Dies ist unabhängig von ggf. Lua Skripten und fragt immer nach "Char".
+    -- Hintergrund ist gewünschtes "einfaches, out-of-the-box" Erlebnis bei vielen Spielen.
+    -- Passt aber nicht zu MG.char also werden wir den Inhalt wieder löschen.
+    -- Damit wir nicht dreimal löschen, hier ein Timer mit Wartezeit von null Sekunden.
+
+    if not GMCP_Char_Timer then 
+      GMCP_Char_Timer = tempTimer(0, function()
+        GMCP_Char_Timer = nil
+        gmcp.Char = nil
+        -- debugText("GMCP Char deaktiviert!")
+      end)
+    end 
+  end
+
+registerAnonymousEventHandler("sysProtocolEnabled", "initGMCP")
+registerAnonymousEventHandler("gmcp.Char", "GMCP_Char_deaktivieren")
