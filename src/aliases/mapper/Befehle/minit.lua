19bac72f91e6200bd0330f5bef4b455d3a461729
Matthias Faust <Fausti@users.noreply.github.com>
2023-01-31 11:31:32 +0100

    Update minit.lua (#13)
    * Update minit.lua
    
    * Ein not zuviel...
    
    * Fix: Check auf GMCP Daten
    
    * fix: erster Raum ohne Hash
    
    * fix: Mapmode auf auto nach minit

--- src/aliases/mapper/Befehle/minit.lua
+++ src/aliases/mapper/Befehle/minit.lua
@@ -1,16 +1,64 @@
+-- Dieses Alias setzt die Karte auf Werkszustand zurueck.
 
--- Dies setzt die Karte auf Werkszustand zurueck.
+-- Hilfsfunktion:
+-- prüft ob ein bestimmtes Feld innerhalb einer komplexeren Tabelle exisitert
 
-for i,name in pairs(getRooms()) do
-    deleteRoom(i)
+local function isField(t, s)
+    if t == nil then return false end
+    local t = t
+    for key in s:gmatch('[^.]+') do
+        if t[key] == nil then return false end
+        t = t[key]
+    end
+    return true
 end
 
-addRoom(1)
-setRoomArea(1, findArea("world"))
-setRoomIDbyHash(1, mapper.currentHash)
-mapper.currentRoom = 1
-centerview(1)
+-- vorhandene Räume löschen
+for id, _ in pairs(getRooms()) do
+    deleteRoom(id)
+end
 
-echoM("Neue Map initialisiert.")
-                    
-                
+-- vorhandene Gebiete löschen, ausser "Default Area"
+for _, id in pairs(getAreaTable()) do
+    if id > -1 then
+        deleteArea(id)
+    end
+end
+
+-- currentArea zurücksetzen damit wir wirklich von vorne anfangen
+mapper.currentArea = "world"
+mapper.currentHash = nil
+
+-- MapUserData löschen
+clearMapUserData()
+
+if isField(gmcp, "MG.room.info") then
+    -- ersten Raum aus aktuellen GMCP Daten erstellen
+    local roomData = gmcp.MG.room.info
+    local hash = roomData.id
+
+    mapper.currentHash = hash
+
+    local newRoom = createRoom(mapper.currentArea, hash)
+
+    setRoomName(newRoom, roomData.short)
+
+    -- im neuen Raum alle sichtbaren Ausgänge prüfen und ggf. Stubs erzeugen
+    for _, exitname in pairs(roomData.exits) do
+        addStubExit(newRoom, exitname)
+    end
+
+    mapper.currentRoom = newRoom
+    centerview(newRoom)
+else
+    -- keine Raumdaten vorhanden
+    local newRoom = createRoom(mapper.currentArea, mapper.currentHash)
+    
+    setRoomName(newRoom, "Irgendwo im Nirgendwo")
+
+    mapper.currentRoom = newRoom
+    centerview(newRoom)
+end
+
+echoM("Neue Karte initialisiert.")
+setMapperMode("auto")
