abbe42080fbc04f4939045d65acddaa51b1909ca
Kebap <kebap_spam@gmx.net>
2023-02-05 12:25:08 +0100

    Logik reparieren

--- src/scripts/krrrcks/GUI/initBase.lua
+++ src/scripts/krrrcks/GUI/initBase.lua
@@ -1,6 +1,6 @@
 function initBase() 
-  if not table.is_field(gmcp, "MG.char.base") and 
-         table.is_field(gmcp, "MG.char.info") then return end
+  if not ( table.is_field(gmcp, "MG.char.base") and 
+           table.is_field(gmcp, "MG.char.info") ) then return end
 
   ME.name = gmcp.MG.char.base.name 
   ME.stufe = gmcp.MG.char.info.level
