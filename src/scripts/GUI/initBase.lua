8a5abbacababb83cec8f42a1ec4e15b2d104ba5e
Kebap <kebap_spam@gmx.net>
2023-02-06 02:23:15 +0100

    Resolve merge error

--- src/scripts/GUI/initBase.lua
+++ src/scripts/GUI/initBase.lua
@@ -2,13 +2,12 @@ function initBase()
   if not ( table.is_field(gmcp, "MG.char.base") and 
            table.is_field(gmcp, "MG.char.info") ) then return end
 
-    ME.name = gmcp.MG.char.base.name
-    ME.stufe = gmcp.MG.char.info.level
+  ME.name = gmcp.MG.char.base.name
+  ME.stufe = gmcp.MG.char.info.level
 
-    if not GUI.angezeigt then
-        initGUI()
-        initCharGUI()
-        GUI.angezeigt = true
-    end
+  if not GUI.angezeigt then
+      initGUI()
+      initCharGUI()
+      GUI.angezeigt = true
   end
 end
