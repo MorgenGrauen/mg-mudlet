e9ce59e50a85143c43b1fded769cdfe03731e87e
Kebap <kebap_spam@gmx.net>
2023-02-17 11:29:01 +0100

    Adjustable Chatbox Styling and Profiling

--- src/scripts/GUI/EMCO/initEMCO.lua
+++ src/scripts/GUI/EMCO/initEMCO.lua
@@ -23,6 +23,22 @@ function initChatbox()
         GUI.Chatbox.GUI.AdjCon or
         Adjustable.Container:new({
             name = "GUI.Chatbox.GUI.AdjCon"
+            x = -360, y = 240,
+            padding = 2, width = 360, height = 100% - 240,
+            adjLabelstyle = [[ 
+                border-color: rgba(113, 75, 50, 100%);
+                border-width: 1px; border-style: solid; border-radius: 10px; 
+                background-color:rgba(0, 0, 0, 100%);
+            ]],
+            buttonstyle=[[
+                QLabel{ border-radius: 7px; background-color: rgba(140,140,140,20%);}
+                QLabel::hover{ background-color: rgba(160, 160, 160, 50%);}
+            ]],
+            buttonFontSize = 8,
+            buttonsize = 16,
+            titleText = "Kommunikation",
+            titleTxtColor = "SaddleBrown",
+            attached = "right",
     })  -- Position unterhalb Mapperbox!
 
     GUI.Chatbox.GUI.EMCO = EMCO:new({
