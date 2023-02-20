4fb2fa78749d601857d99d3a0d56e609401dc91a
Kebap <kebap_spam@gmx.net>
2023-02-20 10:00:17 +0100

    Chatbox (#45)
    * Add emco from demonnic MDK v2.8.0
    
    Source: 
    - https://github.com/demonnic/MDK/
    - https://github.com/demonnic/MDK/releases/tag/v2.8.0
    
    * Rename src/resources/emco.lua to src/resources/emco/emco.lua
    
    * require EMCO (from deep path)
    
    * initEMCO in eigener Datei/Ordner verschieben
    
    * EMCO für Ebenen & Mitteilungen
    
    * Adjustable Container für Chatbox GUI
    
    * Adjustable Chatbox Styling and Profiling
    
    * Code aufräumen
    
    * Chatbox oben und unten anbinden
    
    * Code aufräumen
    
    * Farben angleichen an CharGUI, Mapbox
    
    * CSS Code Layout aufräumen
    
    * CSS einheitlich sortiert
    
    * Debug Tab zeigen, aber nicht auch in Alles
    
    * MapperBox ebenfalls oben/unten verbinden, aber zeitversetzt weil Chatbox da noch fehlt
    
    * Chatbox und Mapperbox bei Installation erschaffen
    
    * Neue Events: Start, Exit und Uninstall
    
    * (Un)Install bemerkt nun auch Module
    
    * EMCO besser einbinden
    
    * EMCO braucht loggingconsole?
    
    * EMCO braucht nun auch daemontools
    
    * Layout aufräumen
    
    * onInstall laufe erst nachdem alles geladen ist
    
    * GUI definieren falls noch nicht
    
    * Ordner umbenennen ändert Ausführungsreihenfolge -.-
    
    * sortiere demonnics MDK Dateien besser
    
    * schwarz statt lila Schrift im aktiven Tab
    
    * Mitteilungen in Chatbox anzeigen
    
    * Ebenen in Chatbox anzeigen
    
    * Ebenen nicht doppelt anzeigen

--- src/scripts/GUI/Karte/initMapperbox.lua
+++ src/scripts/GUI/Karte/initMapperbox.lua
@@ -1,18 +1,19 @@
-function initMapperBox()
-    GUI.Karte = 
-    -- GUI.Karte or 
+function initMapperbox()
+    GUI = GUI or {}
+    GUI.Karte =
+    -- GUI.Karte or
     {}
 
-    GUI.Karte.testCon = 
-    -- GUI.Karte.testCon or 
+    GUI.Karte.AdjCon =
+    -- GUI.Karte.AdjCon or
     Adjustable.Container:new(
         {
         name = "MapFrame",
         x = -360, y = 0,
         padding = 2, width = 360, height = 240,
-        adjLabelstyle = [[ 
+        adjLabelstyle = [[
             border-color: rgba(113, 75, 50, 100%);
-            border-width: 1px; border-style: solid; border-radius: 10px; 
+            border-width: 1px; border-style: solid; border-radius: 10px;
             background-color:rgba(0, 0, 0, 100%);
         ]],
         buttonstyle=[[
@@ -26,18 +27,24 @@ function initMapperBox()
         attached = "right",
         }
     )
-    
+
     GUI.Karte.mapper =
     --  GUI.Karte.mapper or
     Geyser.Mapper:new(
         {
         name = "Mapper",
-        x = 0, y = 12, 
-        width = "100%", 
-        height = "100%-17", 
-        }, 
-        GUI.Karte.testCon
+        x = "0", y = "12",
+        width = "100%",
+        height = "100%-17px",
+        },
+        GUI.Karte.AdjCon
     )
-    
+
     setMapZoom(5)
+
+    tempTimer(0, function()
+        GUI.Karte.AdjCon:connectToBorder("top")
+        GUI.Karte.AdjCon:connectToBorder("bottom")
+    end)
+
 end
---
+++ src/scripts/Setup/onInstall.lua
@@ -0,0 +1,13 @@
+function onInstall(_, packageName)
+    -- alles was NUR direkt nach der Installation des MG Pakets ausgeführt werden soll
+    if not packageName == "@PKGNAME@" then return end
+
+    tempTimer(0, function()
+        initGMCP("", "GMCP")
+
+        initMapperbox()
+        initChatbox()
+
+        cecho("<LimeGreen>[  OK  ]  -<gold> Dein Mudlet-Paket fürs MorgenGrauen wurde erfolgreich installiert.\n")
+    end)
+end
