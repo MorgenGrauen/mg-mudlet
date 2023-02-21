e51d8b182d3e3b1f2f86e7f73616b36466014db3
Kebap <kebap_spam@gmx.net>
2023-02-21 09:57:48 +0100

    Lade Startgebiet in eigener Funktion

--- src/scripts/mapper/ladeStartgebiet.lua
+++ src/scripts/mapper/ladeStartgebiet.lua
@@ -1,6 +1,5 @@
 function ladeStartgebiet()
     -- Karte mit Startgebieten laden
-    -- TODO: Alles folgende in eigenes Skript auslagern, das von hier aufgerufen wird.
 
     -- TODO: Passende Karte je nach Rasse laden. Nicht mehr die v8 Karte bereitstellen.
     loadMap(getMudletHomeDir() .. "/@PKGNAME@/" .. "map/anfaenger v8.dat")
