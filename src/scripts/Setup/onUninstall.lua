fca5b2e8ab02b18806e8915325e161b46d35f926
Kebap <kebap_spam@gmx.net>
2023-02-22 23:51:15 +0100

    Alleine nicht ausreichend, eher störend

--- src/scripts/Setup/onUninstall.lua
+++ src/scripts/Setup/onUninstall.lua
@@ -2,6 +2,8 @@ function onUninstall(_, packageName)
     -- alles was direkt vor der Deinstallation des MG Pakets ausgeführt werden soll
     if not packageName == "@PKGNAME@" then return end
 
-    setBorderSizes(0)
+    -- setBorderSizes(0)
+    -- Kartenfenster ausblenden (aber so, dass Neuinstallation es wieder einblenden kann)
+    -- Kommunikationsfenster ausblenden
 
 end
