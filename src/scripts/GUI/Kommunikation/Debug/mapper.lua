1fa20e84d7eafef7d2ea3a9f56717aee84da8643
Kebap <kebap_spam@gmx.net>
2023-02-25 15:11:52 +0100

    Mapper Meldungen optisch angleichen

--- src/scripts/GUI/Kommunikation/Debug/mapper.lua
+++ src/scripts/GUI/Kommunikation/Debug/mapper.lua
@@ -1,6 +1,6 @@
 function echoM(str)
     -- TODO: Mit faerbeText() harmonisieren..!
     fg(mapperconf.color)
-    echo("[MAPPER] " .. str .. "\n")
+    echo("[MAPPER]  - " .. str .. "\n")
     resetFormat()
 end
