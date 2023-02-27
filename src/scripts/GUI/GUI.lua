d2ffea5271f1022c0c8967c7a6cc7ed7cdb50280
Kebap <kebap_spam@gmx.net>
2023-02-27 10:42:51 +0100

    Kommentar war unvollständig

--- src/scripts/GUI/GUI.lua
+++ src/scripts/GUI/GUI.lua
@@ -1,7 +1,9 @@
 -- Hierin sammel ich alles, was für die GUI von Wichtigkeit sein könnte
 
 --[[
-Etwas trickreich ist folgendes:
-Der "Script name" (bspw. "zeigeVitaldaten" muss genau dem Funktionsnamen für
-diese Funktion entsprechen, sonst tut das nicht).
+Um eine Funktion durch ein Event auslösen zu lassen, gibt es zwei Möglichkeiten:
+- Alle gewünschten Events in der GUI eintragen und auf Plus drücken. Dann muss aber 
+  der "Script name" (bspw. "initGMCP" muss dem Namen einer Funktion in diesem Script
+  entsprechen, sonst tut das nicht), oder
+- Selbst im Code per registerAnonymousEventHandler() die Verbindung herstellen.
 ]]--
