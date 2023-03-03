cb4fcb5d39de58785e96964b0166fb6da5139706
Kebap <kebap_spam@gmx.net>
2023-03-03 01:15:13 +0100

    Layout angleichen

--- src/scripts/GUI/Karte/initMapperbox.lua
+++ src/scripts/GUI/Karte/initMapperbox.lua
@@ -4,6 +4,15 @@ function initMapperbox()
     -- GUI.Karte or
     {}
 
+    local title_style = [[
+        border-color: rgba(113, 75, 50, 100%);
+        border-width: 1px; border-style: solid; border-radius: 10px;
+        background-color:rgba(0, 0, 0, 100%);
+    ]]
+    local button_style = [[
+        QLabel{ border-radius: 7px; background-color: rgba(140,140,140,20%);}
+        QLabel::hover{ background-color: rgba(160, 160, 160, 50%);}
+    ]]
     GUI.Karte.Frame =
     -- GUI.Karte.Frame or
     Adjustable.Container:new(
@@ -11,15 +20,9 @@ function initMapperbox()
         name = "MapFrame",
         x = -360, y = 0,
         padding = 2, width = 360, height = 240,
-        adjLabelstyle = [[
-            border-color: rgba(113, 75, 50, 100%);
-            border-width: 1px; border-style: solid; border-radius: 10px;
-            background-color:rgba(0, 0, 0, 100%);
-        ]],
-        buttonstyle=[[
-            QLabel{ border-radius: 7px; background-color: rgba(140,140,140,20%);}
-            QLabel::hover{ background-color: rgba(160, 160, 160, 50%);}
-        ]],
+        adjLabelstyle = title_style,
+        buttonstyle = button_style,
+        lockStyle = "border",
         buttonFontSize = 8,
         buttonsize = 16,
         titleText = "Karte",
@@ -27,6 +30,7 @@ function initMapperbox()
         attached = "right",
         }
     )
+    GUI.Karte.Frame:changeMenuStyle("dark")
 
     GUI.Karte.Mapper =
     --  GUI.Karte.Mapper or
@@ -34,8 +38,7 @@ function initMapperbox()
         {
         name = "Mapper",
         x = "0", y = "12",
-        width = "100%",
-        height = "100%-17px",
+        width = "100%", height = "100%-17px",
         },
         GUI.Karte.Frame
     )
