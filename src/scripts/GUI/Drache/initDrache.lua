1ad0f66bfda8df31ccc1a2c6ffbe73d0eaf885a4
Kebap <kebap_spam@gmx.net>
2023-03-03 01:52:25 +0100

    Drache zeigt Hilfe, GUI Reset

---
+++ src/scripts/GUI/Drache/initDrache.lua
@@ -0,0 +1,72 @@
+GUI = GUI or {}
+GUI.Drache = GUI.Drache or {
+    Config = {
+        ImageFile = getMudletHomeDir() .. "/@PKGNAME@/drache/drache_bunt_60x50_transparent.png",
+        ImageWidth = 60,
+        ImageHeight = 50,
+        PaddingWidth = 30,
+        PaddingHeight = 10,
+        MenuFontAlignment = "c", -- centered, all options: "clr",.
+        MenuFontSize = 8,
+        MenuHeight = 20,
+        MenuNames =
+          {
+            "Hilfe",
+            "GUI",
+            {
+              "Reset"
+            }
+          },
+        MenuActions = "defined below"
+    }
+}
+GUI.Drache.Actions = GUI.Drache.Actions or {}
+
+function initDrache()
+
+    -- Configure layout
+    local PaddingWidth = GUI.Drache.Config.PaddingWidth
+    local PaddingHeight = GUI.Drache.Config.PaddingHeight
+    local ImageWidth = GUI.Drache.Config.ImageWidth
+    local ImageHeight = GUI.Drache.Config.ImageHeight
+    local ImagePosX = -PaddingWidth - ImageWidth - getBorderRight()
+    local ImagePosY = PaddingHeight + getBorderTop()
+
+    -- Setup the menu icon
+    GUI.Drache.Frame = Geyser.Label:new({
+        name = "GUI.Drache.Frame",
+        x = ImagePosX,
+        y = ImagePosY,
+        width = ImageWidth,
+        height = ImageHeight,
+        fillBg = 0
+    })
+    GUI.Drache.Frame:setTiledBackgroundImage(GUI.Drache.Config.ImageFile)
+
+    -- Fill the menu
+    local MenuFontAlignment = GUI.Drache.Config.MenuFontAlignment
+    local MenuFontSize = GUI.Drache.Config.MenuFontSize
+    GUI.Drache.Frame:createRightClickMenu({
+        MenuItems = GUI.Drache.Config.MenuNames,
+        Style = "dark",
+        MenuWidth = -ImagePosX + 5,
+        MenuFormat = MenuFontAlignment .. MenuFontSize,
+        MenuHeight = GUI.Drache.Config.MenuHeight,
+    })
+
+    -- Connect the actions
+    GUI.Drache.Frame:setMenuAction("Hilfe", GUI.Drache.Actions.Hilfe)
+    GUI.Drache.Frame:setMenuAction("GUI.Reset", GUI.Drache.Actions.GUIReset)
+end
+
+
+function GUI.Drache.dontMove(event, x, y)
+    local PaddingWidth = GUI.Drache.Config.PaddingWidth
+    local PaddingHeight = GUI.Drache.Config.PaddingHeight
+    local ImageWidth = GUI.Drache.Config.ImageWidth
+    local ImagePosX = -PaddingWidth - ImageWidth - getBorderRight()
+    local ImagePosY = PaddingHeight + getBorderTop()
+
+    GUI.Drache.Frame:move(ImagePosX, ImagePosY)
+end
+registerAnonymousEventHandler("sysWindowResizeEvent", GUI.Drache.dontMove, false)
