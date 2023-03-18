a06caa72eb55255d2db6bcbba02159d778610edc
Kebap <kebap_spam@gmx.net>
2023-03-18 09:58:47 +0100

    entferne obsolete Aufrufe

--- src/scripts/GUI/Drache/Actions/GUIReset.lua
+++ src/scripts/GUI/Drache/Actions/GUIReset.lua
@@ -2,12 +2,7 @@ function GUIReset()
 
     resetProfile()
 
-    initGUI() -- Hier noch die alte krrrcks Statuszeile
-    -- TODO: Bald wird initGUI() stattdessen die komplette GUI bauen.
-    -- Dann sind die folgenden Zeilen vmtl. obsolet und Teil von dort:
-    initMapperbox()
-    initChat()
-    initDrache()
+    initGUI()
 
     initGMCP("", "GMCP")
 
