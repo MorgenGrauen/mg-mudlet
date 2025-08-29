520976514f93ed11ed1b69c2f6090a3aefdc9ea2
Kebap <kebap_spam@gmx.net>
2025-08-29 08:42:03 +0200

    Funktionstasten zu Beginn vorbereiten

---
+++ src/scripts/settings/Einstellungen/Funktionstasten.lua
@@ -0,0 +1,11 @@
+
+----------------------------------------------------------------
+-- Benutzerdefinierte Einstellungen fuer die Funktionstasten. --
+----------------------------------------------------------------
+
+-- Standardelegung zu Beginn festlegen
+Hotkeys = Hotkeys or {
+    F1 = "hilfe",
+    F2 = "inventar",
+    F3 = "kwer"
+}
