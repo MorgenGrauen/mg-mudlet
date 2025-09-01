32511910a493bab8370813be5fed947ba3329266
Kebap <kebap_spam@gmx.net>
2025-09-01 11:23:35 +0200

    F1-F8 vorbelegen

--- src/scripts/settings/Einstellungen/Funktionstasten.lua
+++ src/scripts/settings/Einstellungen/Funktionstasten.lua
@@ -6,6 +6,11 @@
 -- Standardelegung zu Beginn festlegen
 Hotkeys = Hotkeys or {
     F1 = "hilfe",
-    F2 = "inventar",
-    F3 = "kwer"
+    F2 = "info",
+    F3 = "kurzinfo",
+    F4 = "inventar",
+    F5 = "kwer",
+    F6 = "zaubertraenke",
+    F7 = "hist",
+    F8 = "-anf Hallo ihr da draussen! Ich probiere gerade die Funktionstasten in Mudlet aus."
 }
