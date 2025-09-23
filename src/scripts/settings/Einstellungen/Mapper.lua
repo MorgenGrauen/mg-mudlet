d9654e6e4befe159ef64b0de4f8beb6e5dd47777
Kebap <kebap_spam@gmx.net>
2025-09-23 07:36:17 +0200

    space statt tabs

--- src/scripts/settings/Einstellungen/Mapper.lua
+++ src/scripts/settings/Einstellungen/Mapper.lua
@@ -11,14 +11,14 @@ mapperconf.scale = mapperconf.scale or 2
 -- Standardfarben neuer Raeume je nach Region - Mudlet bietet dazu Environments an
 mapperconf.region_env = mapperconf.region_env or {
     { dschungel = 262 }, -- #008080 
-    { ebene 	= 266 }, -- #00ff00 
+    { ebene     = 266 }, -- #00ff00 
     { fernwest 	= 257 }, -- #800000 
     { gebirge 	= 272 }, -- #808080 
-    { inseln 	= 268 }, -- #0000ff 
-    { polar 	= 269 }, -- #00ffff 
-    { unbekannt = 271 }, -- #ffffff
+    { inseln    = 268 }, -- #0000ff 
+    { polar     = 269 }, -- #00ffff 
+    { unbekannt = 271 }, -- #ffffff 
     { unterwelt = 264 }, -- #000000 
-    { vland 	= 267 }, -- #ffff00 
+    { vland     = 267 }, -- #ffff00 
     { wald 	    = 258 }, -- #008000 
-    { wueste 	= 259 }, -- #808000 
+    { wueste    = 259 }, -- #808000 
 }
